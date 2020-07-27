15e2bc3628f7d47225717f0aca5f5f2bd2e80ddc
Ysj <38207969+Ysj001@users.noreply.github.com>
2020-07-27 21:43:38 +0800

    完善曲线效果计算，更新 demo

--- app/src/main/java/com/ysj/ui/floatingbottomnavigator/MainActivity.kt
+++ app/src/main/java/com/ysj/ui/floatingbottomnavigator/MainActivity.kt
@@ -1,61 +1,126 @@
 package com.ysj.ui.floatingbottomnavigator
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.View
+import android.view.animation.LinearInterpolator
+import android.view.animation.OvershootInterpolator
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.ysj.ui.floatingbottomnavigation.FloatingBottomNavigation
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bottomNav: FloatingBottomNavigation
-    private lateinit var v1: View
-    private lateinit var v2: View
-    private lateinit var v3: View
+    companion object {
+        private var bottomNavHeight = 0
+    }
 
+    private var debugEnable = false
+
+    // 动画范围
+    private var mRang = Pair(1f, 1.4f)
     private var oldView: View? = null
+    private var oldAnim: ValueAnimator? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bottomNav = findViewById(R.id.bottomNav)
-        v1 = findViewById(R.id.view1)
-        v2 = findViewById(R.id.view2)
-        v3 = findViewById(R.id.view3)
         bottomNav.apply {
-            post {
-                bindView(v1)
-                bindView(v2)
-                bindView(v3)
-                setDebugEnable(true)
-                test(v1)
+            if (bottomNavHeight != 0) layoutParams.height = bottomNavHeight
+            tvMultiple.text = ("Max multiple: ${mRang.second}")
+            tvMaxFloating.text = ("Max Floating: ${getMaxFloating()}(px)")
+            tvDividerStrokeWidth.text = ("Divider Stroke Width: ${getDividerStrokeWidth()}(px)")
+            tvBottomNavHeight.text = ("Bottom Navigation Height: ${layoutParams.height}(px)")
+            bindView(view1)
+            bindView(view2)
+            bindView(view3)
+            post { onNavClick(view1) }
+        }
+    }
+
+    fun onDebugClick(view: View) {
+        debugEnable = !debugEnable
+        bottomNav.setDebugEnable(debugEnable)
+        (view as TextView).text = if (debugEnable) "debug on" else "debug-off"
+    }
+
+    fun onNavClick(view: View) {
+        if (view == oldView) return
+        // 先还原老的
+        oldAnim?.apply {
+            if (isRunning) return
+            duration = 200
+            interpolator = LinearInterpolator()
+            reverse()
+        }
+        // 新点击的
+        val y = view.y
+        val offset = view.height * (mRang.second - mRang.first)
+        ValueAnimator.ofFloat(mRang.first, mRang.second).apply {
+            oldAnim = this
+            duration = 400
+            interpolator = OvershootInterpolator(3f)
+            addUpdateListener {
+                val value = it.animatedValue as Float
+                view.scaleX = value
+                view.scaleY = value
+                view.y = y - offset * (value - 1)
             }
-        }
+        }.start()
+        oldView = view
     }
 
-    fun test(view: View) {
-        val id = view.id
-        oldView?.apply {
-            // 先还原老的
-            animate().setDuration(300)
-                .scaleX(scaleX / 1.2f)
-                .scaleY(scaleY / 1.2f)
-                .y(y + 24)
-                .start()
-        }
-
-        // 新的
-        view.animate().setDuration(300)
-            .scaleX(view.scaleX * 1.2f)
-            .scaleY(view.scaleY * 1.2f)
-            .y(view.y - 24)
-            .setListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    super.onAnimationEnd(animation)
-                    oldView = view
-                }
-            })
-            .start()
+    fun onMaxMultiplePlus(view: View) {
+        mRang = Pair(mRang.first, mRang.second + 0.1f)
+        tvMultiple.text = ("Max multiple: ${mRang.second}")
     }
+
+    fun onMaxSizeMinus(view: View) {
+        mRang = Pair(mRang.first, mRang.second - 0.1f)
+        tvMultiple.text = ("Max multiple: ${mRang.second}")
+    }
+
+    fun onDividerStrokeWidthPlus(view: View) {
+        val value = bottomNav.getDividerStrokeWidth()
+        bottomNav.setDividerStrokeWidth(value + 1)
+        tvDividerStrokeWidth.text = ("Divider Stroke Width: ${value + 1}(px)")
+    }
+
+    fun onDividerStrokeWidthMinus(view: View) {
+        val value = bottomNav.getDividerStrokeWidth()
+        bottomNav.setDividerStrokeWidth(value - 1)
+        tvDividerStrokeWidth.text = ("Divider Stroke Width: ${value - 1}(px)")
+    }
+
+    fun onMaxFloatingPlus(view: View) {
+        val value = bottomNav.getMaxFloating()
+        bottomNav.setMaxFloating(value + 1)
+        tvMaxFloating.text = ("Max Floating: ${value + 1}(px)")
+    }
+
+    fun onMaxFloatingMinus(view: View) {
+        val value = bottomNav.getMaxFloating()
+        bottomNav.setMaxFloating(value - 1)
+        tvMaxFloating.text = ("Max Floating: ${value - 1}(px)")
+    }
+
+    fun onBottomNavHeightPlus(view: View) {
+        val height = bottomNav.layoutParams.height
+        bottomNav.layoutParams.height = height + 10
+        bottomNavHeight = bottomNav.layoutParams.height
+        recreate()
+    }
+
+    fun onBottomNavHeightMinus(view: View) {
+        val height = bottomNav.layoutParams.height
+        bottomNav.layoutParams.height = height - 10
+        bottomNavHeight = bottomNav.layoutParams.height
+        recreate()
+    }
+
+    fun onResetClick(view: View) {
+        bottomNavHeight = 0
+        recreate()
+    }
+
 }
--- floatingbottomnavigation/src/main/java/com/ysj/ui/floatingbottomnavigation/FloatingBottomNavigation.kt
+++ floatingbottomnavigation/src/main/java/com/ysj/ui/floatingbottomnavigation/FloatingBottomNavigation.kt
@@ -1,7 +1,10 @@
 package com.ysj.ui.floatingbottomnavigation
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
@@ -18,21 +21,16 @@ import kotlin.math.sqrt
  */
 class FloatingBottomNavigation : LinearLayout {
 
-    private val TAG = "BottomNavigation"
-
     // =================== 绘制参数 =====================
 
     // 分割线颜色
-    private val mDividerColor = Color.GREEN
+    private var mDividerColor: Int
 
-    // 分割线到 view 的间距
-    private var mDividerPadding: Float
-
-    // 分割线高度
-    private var mDividerHeight: Float
+    // 分割线画笔宽度
+    private var mDividerStrokeWidth: Float
 
     // 最大浮动
-    private val mMaxFloat: Float
+    private var mMaxFloating: Float
 
     // =================================================
 
@@ -59,24 +57,25 @@ class FloatingBottomNavigation : LinearLayout {
 
     constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
             : super(context, attrs, defStyleAttr) {
-        compatibility()
-        // 使其会绘制自己
-        setWillNotDraw(false)
         // 加载自定义属性
         context.obtainStyledAttributes(attrs, R.styleable.FloatingBottomNavigation).apply {
-            mDividerHeight = getDimension(R.styleable.FloatingBottomNavigation_dividerHeight, 0f)
-            mDividerPadding = getDimension(R.styleable.FloatingBottomNavigation_dividerPadding, 0f)
-            mMaxFloat = getDimension(R.styleable.FloatingBottomNavigation_maxFloat, 0f)
+            mMaxFloating = getDimension(R.styleable.FloatingBottomNavigation_maxFloating, 0f)
+            mDividerStrokeWidth =
+                getDimension(R.styleable.FloatingBottomNavigation_dividerStrokeWidth, 0f)
+            mDividerColor = getColor(R.styleable.FloatingBottomNavigation_dividerColor, Color.GRAY)
         }.recycle()
         init()
     }
 
     override fun draw(canvas: Canvas) {
+        canvas.drawColor(Color.TRANSPARENT)
+        mDebugPaint?.apply {
+            canvas.drawLine(0f, 0f, width.toFloat(), 0f, this)
+        }
         // 因为需要裁切 view 的背景，因此该调用需要在 super.draw(canvas) 前
         drawDivider(canvas)
         super.draw(canvas)
         mDebugDraws?.forEach { it.run() }
-//        drawDivider2(canvas)
     }
 
     /**
@@ -85,6 +84,59 @@ class FloatingBottomNavigation : LinearLayout {
     fun bindView(view: View?) {
         view?.apply { mBindViews.add(this) }
     }
+
+    /**
+     * 解绑一个 View
+     */
+    fun unBindView(view: View?) {
+        view?.apply { mBindViews.remove(this) }
+    }
+
+    /**
+     * 设置分割线颜色
+     *
+     * @attr ref R.styleable#FloatingBottomNavigation_dividerColor
+     */
+    fun setDividerColor(color: Int) {
+        mDividerColor = color
+        invalidate()
+    }
+
+    /**
+     * 获取分割线的颜色
+     */
+    fun getDividerColor() = mDividerColor
+
+    /**
+     * 设置分割线的画笔粗细（px）
+     *
+     * @attr ref R.styleable#FloatingBottomNavigation_dividerStrokeWidth
+     */
+    fun setDividerStrokeWidth(dividerStrokeWidth: Float) {
+        mDividerStrokeWidth = dividerStrokeWidth
+        mDividerPaint.strokeWidth = mDividerStrokeWidth
+        invalidate()
+    }
+
+    /**
+     * 获取分割线画笔的粗细（px）
+     */
+    fun getDividerStrokeWidth() = mDividerStrokeWidth
+
+    /**
+     * 设置最大浮动范围（px）
+     *
+     * @attr ref R.styleable#FloatingBottomNavigation_maxFloating
+     */
+    fun setMaxFloating(maxFloat: Float) {
+        mMaxFloating = maxFloat
+        invalidate()
+    }
+
+    /**
+     * 获取最大浮动范围（px）
+     */
+    fun getMaxFloating() = mMaxFloating
 
     /**
      * 是否开启 debug，开启后可以看到绘制辅助视图
@@ -99,13 +151,14 @@ class FloatingBottomNavigation : LinearLayout {
             isAntiAlias = true
             strokeWidth = 4f
         } else null
+        invalidate()
     }
 
     private fun drawDivider(canvas: Canvas) {
         mDebugDraws?.clear()
 
         mDividerPath.reset()
-        mDividerPath.moveTo(-mDividerHeight, mMaxFloat)
+        mDividerPath.moveTo(-mDividerStrokeWidth, mMaxFloating)
         mBindViews.forEach {
             val childRealX = getChildRealX(it)
             val childRealY = getChildRealY(it)
@@ -116,43 +169,44 @@ class FloatingBottomNavigation : LinearLayout {
             val x = childRealX + it.width / 2
             val y = childRealY + it.height / 2
             // 外圆半径
-            val radius = sqrt((childRealWidth / 2 * childRealWidth / 2
-                    + childRealHeight / 2 * childRealHeight / 2).toDouble()).toFloat()
+            val radius = sqrt(
+                (childRealWidth / 2 * childRealWidth / 2
+                        + childRealHeight / 2 * childRealHeight / 2).toDouble()
+            ).toFloat()
             // 控制点 Y 坐标
-            val controlPointY = if (y - radius >= mMaxFloat) mMaxFloat
-            else y - radius - abs(y - radius - mMaxFloat)
-
-            val p = (mMaxFloat - controlPointY) / radius
+            val controlPointY = if (y - radius >= mMaxFloating) mMaxFloating
+            else y - radius - abs(y - radius - mMaxFloating)
+            // 达到半径的比例
+            val p = (mMaxFloating - controlPointY) / radius
             val r = if (p <= 0) 0f else (radius - childRealWidth / 2) * p / 2
 
             mDebugDraws?.add(Runnable {
                 mDebugPaint?.apply {
                     val dr = 8f
                     // 第一段，原始点：canvas.drawCircle(x - radius, mMaxFloat, dr, this)
-                    canvas.drawCircle(x - radius - r, mMaxFloat, dr, this)
-                    canvas.drawCircle(x - radius + r * 2, mMaxFloat - r, dr, this)
-                    canvas.drawCircle(x - radius + r, mMaxFloat, dr, this)
+                    canvas.drawCircle(x - radius - r, mMaxFloating, dr, this)
+                    canvas.drawCircle(x - radius, mMaxFloating, dr, this)
+                    canvas.drawCircle(x - radius + r, mMaxFloating - r, dr, this)
                     // 第二段控制点
                     canvas.drawCircle(x, controlPointY, dr, this)
                     // 第三段，原始点：canvas.drawCircle(x + radius, mMaxFloat, dr, this)
-                    canvas.drawCircle(x + radius - r * 2, mMaxFloat - r, dr, this)
-                    canvas.drawCircle(x + radius - r, mMaxFloat, dr, this)
-                    canvas.drawCircle(x + radius + r, mMaxFloat, dr, this)
+                    canvas.drawCircle(x + radius - r, mMaxFloating - r, dr, this)
+                    canvas.drawCircle(x + radius, mMaxFloating, dr, this)
+                    canvas.drawCircle(x + radius + r, mMaxFloating, dr, this)
 
                     canvas.drawCircle(x, y, radius, this)
                 }
             })
 //            mDividerPath.lineTo(x - radius, mMaxFloat)
-            mDividerPath.lineTo(x - radius - r, mMaxFloat)
+            mDividerPath.lineTo(x - radius - r, mMaxFloating)
 //            mDividerPath.quadTo(x, controlPointY, x + radius, mMaxFloat)
-            mDividerPath.quadTo(x - radius + r, mMaxFloat, x - radius + r * 2, mMaxFloat - r)
-            mDividerPath.quadTo(x, controlPointY, x + radius - r * 2, mMaxFloat - r)
-            mDividerPath.quadTo(x + radius - r, mMaxFloat, x + radius + r, mMaxFloat)
-
+            mDividerPath.quadTo(x - radius, mMaxFloating, x - radius + r, mMaxFloating - r)
+            mDividerPath.quadTo(x, controlPointY, x + radius - r, mMaxFloating - r)
+            mDividerPath.quadTo(x + radius, mMaxFloating, x + radius + r, mMaxFloating)
         }
-        mDividerPath.lineTo(width + mDividerHeight, mMaxFloat)
-        mDividerPath.lineTo(width + mDividerHeight, height + mDividerHeight)
-        mDividerPath.lineTo(-mDividerHeight, height + mDividerHeight)
+        mDividerPath.lineTo(width + mDividerStrokeWidth, mMaxFloating)
+        mDividerPath.lineTo(width + mDividerStrokeWidth, height + mDividerStrokeWidth)
+        mDividerPath.lineTo(-mDividerStrokeWidth, height + mDividerStrokeWidth)
         mDividerPath.close()
 
         canvas.drawPath(mDividerPath, mDividerPaint)
@@ -160,44 +214,6 @@ class FloatingBottomNavigation : LinearLayout {
         mDebugDraws?.forEach { it.run() }
 
         canvas.clipPath(mDividerPath)
-    }
-
-    @Deprecated("一种不完美的实现思路，保留下来可以看看", ReplaceWith("drawDivider()"))
-    private fun drawDivider2(canvas: Canvas) {
-        val mFloatingPath = Path()
-        mFloatingPath.reset()
-        mDividerPath.reset()
-
-        mDividerPath.moveTo(-mDividerHeight, mMaxFloat)
-        mDividerPath.lineTo(width + mDividerHeight, mMaxFloat)
-        mDividerPath.lineTo(width + mDividerHeight, height + mDividerHeight)
-        mDividerPath.lineTo(-mDividerHeight, height + mDividerHeight)
-        mDividerPath.close()
-
-        mBindViews.forEach {
-            val childRealX = getChildRealX(it)
-            val childRealY = getChildRealY(it)
-            // 缩放后的宽高
-            val childRealWidth = it.width * it.scaleX
-            val childRealHeight = it.height * it.scaleY
-            // 中心坐标
-            val x = childRealX + it.width / 2
-            val y = childRealY + it.height / 2
-            // 外圆半径
-            val radius = sqrt((childRealWidth / 2 * childRealWidth / 2
-                    + childRealHeight / 2 * childRealHeight / 2).toDouble()).toFloat()
-
-            mFloatingPath.moveTo(childRealX, childRealY + it.height / 2)
-            mFloatingPath.addCircle(x, y, radius, Path.Direction.CW)
-            mFloatingPath.moveTo(childRealX + it.width, mMaxFloat)
-        }
-
-        canvas.save()
-        canvas.clipPath(mDividerPath, Region.Op.DIFFERENCE)
-        canvas.clipPath(mFloatingPath, Region.Op.DIFFERENCE)
-        canvas.drawPath(mDividerPath, mDividerPaint)
-        canvas.drawPath(mFloatingPath, mDividerPaint)
-        canvas.restore()
     }
 
     /**
@@ -238,11 +254,16 @@ class FloatingBottomNavigation : LinearLayout {
             flags = Paint.ANTI_ALIAS_FLAG
             isAntiAlias = true
             color = mDividerColor
-            strokeWidth = mDividerHeight
+            strokeWidth = mDividerStrokeWidth
             style = Paint.Style.STROKE
             strokeCap = Paint.Cap.ROUND
             strokeJoin = Paint.Join.ROUND
         }
+        compatibility()
+        // 使其会绘制自己
+        setWillNotDraw(false)
+        // 锁定横向排列
+        orientation = HORIZONTAL
     }
 
     /**
